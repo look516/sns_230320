0208b82b09e0484569d6682aa6fe018c7d1a51b9
look516 <happycatcindy@gmail.com>
2023-07-22 03:04:25 +0900

    230717 회원가입

---
+++ src/main/java/com/sns/user/UserRestController.java
@@ -0,0 +1,64 @@
+package com.sns.user;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sns.common.EncryptUtils;
+import com.sns.user.bo.UserBO;
+import com.sns.user.entity.UserEntity;
+
+@RequestMapping("/user")
+@RestController
+public class UserRestController {
+
+	@Autowired
+	private UserBO userBO;
+
+	/**
+	 * 아이디 중복확인 API
+	 * @param loginId
+	 * @return
+	 */
+	@RequestMapping("/is_duplicated_id")
+	public Map<String, Object> isDuplicatedId(
+			@RequestParam("loginId") String loginId) {
+
+		Map<String, Object> result = new HashMap<>();
+		result.put("isDuplicatedId", false);
+
+		// select
+		UserEntity userEntity = userBO.getUserEntityByLoginId(loginId);
+		result.put("code", 1);
+
+		if (userEntity != null) {
+			result.put("isDuplicatedId", true);
+		} 
+
+		return result;
+	}
+	
+	@PostMapping("/sign_up")
+	public Map<String, Object> signUp(
+			@RequestParam("loginId") String loginId,
+			@RequestParam("password") String password,
+			@RequestParam("name") String name,
+			@RequestParam("email") String email) {
+
+		// 비밀번호 해싱
+		String hashedPassword = EncryptUtils.md5(password);
+
+		// db insert
+
+		// 응답
+		Map<String, Object> result = new HashMap<>();
+		result.put("code", 1);
+		result.put("result", "성공");
+		return result;
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/include/header.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<div class="h-100 header bg-info d-flex justify-content-between">
+	<%-- logo --%>
+	<div class="logo d-flex align-items-center">
+		<h1 class="text-white ml-3">Byeolstargram</h1>
+	</div>
+
+	<%-- 로그인 정보 --%>
+	<div class="login-info d-flex align-items-center mr-5">
+		<span class="text-white">바다님 안녕하세요</span>
+		<a href="#" class="ml-2 text-white font-weight-bold">로그아웃</a>
+	</div>
+</div>
